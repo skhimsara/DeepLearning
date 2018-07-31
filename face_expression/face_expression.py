8c0a54be32ef02d8a12c59282eb840c7d5b8c31e
skhimsara <skhimsara@gmail.com>
2018-07-30 22:20:54 -0700

    Face Expression

---
+++ face_expression/face_expression.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 30 19:18:48 2018
+
+@author: sidnpoo
+"""
+
+DATA_PATH="/Users/sidnpoo/Downloads/MLStuff_DoNotDelete/_DATASETS_/face_expresson/"
+
